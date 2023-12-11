0b3c7f10cc74713e36343916c89df0d546b61686
Karthik K <111484706+KAVILIKARTHIK1@users.noreply.github.com>
2023-12-11 11:18:39 +0530

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
-let voiceSelect = document.querySelector("select");
-window.speechSynthesis.onvoiceschanged = () => {
+let voiceSelect = document.getElementById("voiceSelect");
+let textInput = document.getElementById("textInput");
+
+function populateVoiceList() {
     voices = window.speechSynthesis.getVoices();
+    voiceSelect.innerHTML = "";
+
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name, i);
+        voiceSelect.appendChild(option);
+    });
+
     speech.voice = voices[0];
+}
 
-    voices.forEach((voice, i) => (voiceSelect.options[i]) = new Option(voice.name, i));
+window.addEventListener("load", () => {
+    populateVoiceList();
+});
 
-};
+window.speechSynthesis.onvoiceschanged = populateVoiceList;
+
 voiceSelect.addEventListener("change", () => {
     speech.voice = voices[voiceSelect.value];
 });
-document.querySelector("button").addEventListener("click", () => {
-    speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
 
+document.getElementById("listenButton").addEventListener("click", () => {
+    speech.text = textInput.value;
+    window.speechSynthesis.speak(speech);
+});
+
+// Clear the text area on page reload
+window.addEventListener("beforeunload", () => {
+    textInput.value = "";
 });
